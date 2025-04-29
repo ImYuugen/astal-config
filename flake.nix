07151d7467ee5d254c75a3ac667b5ade335ef559
Yuugen <yuugenssb@proton.me>
2025-04-29 04:41:08 +0200

    feat(flake): add dependencies
    Signed-off-by: Yuugen <yuugenssb@proton.me>

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Dev env and package for my astal configuration.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    astal = {
+      url = "github:aylur/astal";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, astal, ags }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default = pkgs.stdenvNoCC.mkDerivation rec {
+      name = "my-shell";
+      src = ./.;
+
+      nativeBuildInputs = [
+        ags.packages.${system}.default
+        pkgs.wrapGAppsHook
+        pkgs.gobject-introspection
+      ];
+
+      buildInputs = with astal.packages.${system}; [
+      ];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        ags bundle app.ts $out/bin/${name}
+      '';
+    };
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodePackages_latest.typescript-language-server
+        nodePackages_latest.prettier
+        dart-sass
+        vscode-langservers-extracted
+        ags.packages.${system}.agsFull
+      ];
+    };
+  };
+}
